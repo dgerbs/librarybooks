d13e90bae8579b89b48884fa0bcae7adcd666762
Doug Gerber <dojoh@outlook.com>
2013-09-07 15:14:20 -0400

    Add CSV Books

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,23 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+require 'csv'
+
+CSV.foreach("db/books", headers: true) do |row|
+	title = row[0]
+	author = row[1]
+	genre = row[2]
+	year = row[3]
+	book.title = title
+	book.author = author
+	book.genre = genre
+	book.year = year
+end
+
+
+# title,author,genre,year
+# apple,washington,mystery,1801
+# orange,clementine,drama,1923
+# cherry,lambert,scifi,2002
+# grape,concord,fiction,1957
