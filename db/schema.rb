94c8e8d71fe946604878b1fb078383b16e1bbba4
Doug Gerber <dojoh@outlook.com>
2013-09-07 15:16:54 -0400

    Migrate Checkouts. Forgot to migrate prior to adding CSV

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130907184424) do
+ActiveRecord::Schema.define(version: 20130907184915) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,14 @@ ActiveRecord::Schema.define(version: 20130907184424) do
     t.datetime "updated_at"
     t.boolean  "favorite"
     t.integer  "ratings"
+  end
+
+  create_table "checkouts", force: true do |t|
+    t.string   "title"
+    t.string   "author"
+    t.integer  "book_id"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
