1a6a78b421717808959e194032d562e39654db9b
Doug Gerber <dojoh@outlook.com>
2013-09-07 14:46:23 -0400

    Remove Favorite

--- db/migrate/20130907183437_add_favorite_field_to_books.rb
+++ db/migrate/20130907183437_add_favorite_field_to_books.rb
@@ -1,5 +1,12 @@
 class AddFavoriteFieldToBooks < ActiveRecord::Migration
-  def change
-    add_column :books, :favorite, :boolean
+  # def change
+  #   add_column :books, :favorite, :boolean
+  # end
+
+  def up
+  	add_column :books, :favorite, :boolean
   end
+
+  def
+  	remove_column :books, :favorite, :boolean
 end
